edac4910f84d8f7e77c63c7018e60773aba5bd37
Daniele_Santos <anninka2021@gmail.com>
2026-04-27 03:34:26 -0300

    4
    Dicionario em python

---
+++ aula 04.py
@@ -0,0 +1,18 @@
+#dicionario no python
+
+dicionario = {
+
+    'Mãe':'Irene',
+
+    'Irmão 2':'Dayhan',
+
+    'Irmão 1':'Danillo',
+
+    'Tio':'Joelson',
+
+    'Animal':'Lili'
+    }
+
+
+p = dicionario['Tio']
+print(p)

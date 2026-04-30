f13267fdec20979b553277d4422f490ea46d32ec
Daniele_Santos <anninka2021@gmail.com>
2026-04-30 00:48:52 -0300

    7
    função map

---
+++ aula 07.py
@@ -0,0 +1,15 @@
+# funçao map()
+
+kmh = [40, 50, 56, 64, 73, 79, 85]
+
+'''
+mph = []
+for i in kmh:
+    mph.append(i/1.61)
+print(mph)
+
+'''
+
+mph2 = list(map(lambda x:x/1.61, kmh))
+print(mph2)
+

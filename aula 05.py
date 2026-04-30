52654ba9b96ba2c87b21a7089be8c3063734aaad
Daniele_Santos <anninka2021@gmail.com>
2026-04-30 00:46:26 -0300

    aula 05

---
+++ aula 05.py
@@ -0,0 +1,13 @@
+# Manipulando strings
+
+frase = 'Estou comendo rabanetes'
+
+f = frase[2:4]
+
+c = frase.count('a')
+
+l = len(frase)
+
+r = frase.replace('rabanetes','maças')
+
+print(f, c, l, r)
